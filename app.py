7e38d73755cd62592bd4df215671cde0e940e8df
MisoBomb1014 <163790118+MisoBomb1014@users.noreply.github.com>
2024-04-18 11:55:02 -0700

    Updated Portfolio, some work done on sellstock

--- app.py
+++ app.py
@@ -247,9 +247,33 @@ def withdraw():
     return redirect(url_for("wallet"))
 
 
-@app.route("/portfolio")
+@app.route("/portfolio", methods=["GET"])
 def portfolio():
+<<<<<<< Updated upstream
    return render_template('portfolio.html')
+=======
+   user_id = session.get('user_id')
+   portfolio = Portfolio.query.filter_by(userId=user_id)
+   return render_template('portfolio.html', user_id=user_id, portfolio=portfolio)
+
+@app.route("/sellstock", methods=["POST"])
+def portfolio():
+   user_id = session.get('user_id')
+   portfolio = Portfolio.query.filter_by(userId=user_id)
+   stockId = session.get('stockId')
+   price = session.get('price')
+   quantity = session.get('quantity')
+   update_stock = Portfolio(stockId, quantity, price)
+   AmountFromSale = price * quantity
+   user = User.query.filter_by(userId=user_id).first()
+   if user is not None:
+      user.cashBal += int(AmountFromSale)
+      portfolio.stockId -=(update_stock)
+      portfolio.quantity -=(update_stock)
+      db.session.commit()
+      return redirect(url_for("portfolio"))
+
+>>>>>>> Stashed changes
 
 @app.route("/searchstock")
 def searchStock():
